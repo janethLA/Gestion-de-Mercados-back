6e7492d1938efc2a4cebb5b9d799a73115fb829b
janeth <jl715643@gmail.com>
2021-09-23 13:51:01 -0400

    Inicializacion

---
+++ src/main/java/com/dev/sisgestionMercados/SisGestionMercadosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sisgestionMercados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisGestionMercadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisGestionMercadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/sisgestionMercados/SisGestionMercadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sisgestionMercados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisGestionMercadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
